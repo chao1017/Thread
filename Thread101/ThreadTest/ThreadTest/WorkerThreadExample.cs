f21b171b8bb1bc6077099fde590196c98fe02780
chao1017 <chaoshuangjui@gmail.com>
2017-08-04 17:29:11 +0800

    add project

---
+++ Thread101/ThreadTest/ThreadTest/WorkerThreadExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    public class Worker
+    {
+        // This method will be called when the thread is started.
+        public void DoWork()
+        {
+            
+        }
+    }
+
+    public class WorkerThreadExample
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
